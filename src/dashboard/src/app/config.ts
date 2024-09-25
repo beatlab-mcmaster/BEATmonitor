95a48352b30a76f905618c8c2f102955716e31ab
Maya <38996333+mayabflannery@users.noreply.github.com>
2024-09-24 21:46:08 -0400

    docs: add file description and comments

--- src/dashboard/src/app/config.ts
+++ src/dashboard/src/app/config.ts
@@ -1,17 +1,24 @@
+/** config.ts
+ * Author: Maya B. Flannery
+ * Description: Configure global variables in this single file. Settings are
+ * divided into Directories, Server settings, and Parameters for synchronizing
+ * watch time. */
+
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
+// Get root path for app
 const __dirname = join(dirname(fileURLToPath(import.meta.url)), "../");
 
 const settings = {
   // Directory settings
   directory: {
-    watchList: join(__dirname, "watchData/watchList/"),
-    transferredData: join(__dirname, "watchData/transferredData/"),
+    watchList: join(__dirname, "watchData/watchList/"), // .json files are created when Bangle.js watches are found
+    transferredData: join(__dirname, "watchData/transferredData/"), // .csv files are created when data are transferred from watches
   },
 
   // Server settings
-  port: 3001,
+  port: 3001, // Change port if it conflicts with another local process
   routePublic: join(__dirname, "public"),
   routeNodeModules: join(__dirname, "node_modules"),
   index: join(__dirname, "public/dashboard.html"),
