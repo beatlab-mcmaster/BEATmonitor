7c21c87eefab8145dfc922f566d1a21d1987f0c5
Maya <38996333+mayabflannery@users.noreply.github.com>
2025-06-05 00:39:19 -0400

    update: add checks for already processed data; function updates

--- src/postprocess/bangle_process/pipelines.py
+++ src/postprocess/bangle_process/pipelines.py
@@ -1,4 +1,5 @@
 # TODO: import pandas for all files?
+import os
 import pandas as pd
 import neurokit2 as nk
 
@@ -25,34 +26,54 @@ def resample_HR(df, config_dat, save_data=False):
 def resample_PPG(df, config_dat, save_data=False):
     resample_rate = config_dat["pipelines"]["PPG_resample_rate"]
     interpolation_rate = config_dat["pipelines"]["interpolation_rate"]
-    df_dir_out = config_dat["directories"]["data"]["processed"]
-    print(
-        f"Resampling data at: {resample_rate}ms [{interpolation_rate}ms interpolation]"
+    saved_data = (
+        config_dat["directories"]["data"]["processed"]
+        + f"resampled_PPG_{resample_rate}ms.parquet"
     )
-    df.set_index("time", inplace=True)  # TODO: fix need to reset index
-    df_out = (
-        df.groupby("watchId")
-        .resample(f"{interpolation_rate}ms")
-        .interpolate()  # interpolate (works best at 1ms) # TODO
-        .drop("watchId", axis=1)
-        .reset_index(level=0)  # 'ungroup'
-        .groupby("watchId")
-        .resample(f"{resample_rate}ms")
-        .first()  # resample at desired rate
-        .drop("watchId", axis=1)
-        .reset_index(level=0)  # ungroup again
-    )
-    if save_data:
-        df_out.to_parquet(df_dir_out + f"resampled_PPG_{resample_rate}ms.parquet")
+    if os.path.isfile(saved_data):
+        print(f"Using previously computed resampled data from {saved_data}")
+        df_out = pd.read_parquet(saved_data)
+    else:
+        print(
+            f"Resampling data at: {resample_rate}ms [{interpolation_rate}ms interpolation]"
+        )
+        df.set_index("time", inplace=True)  # TODO: fix need to reset index
+        df_out = (
+            df.groupby("watchId")
+            .resample(f"{interpolation_rate}ms")
+            .interpolate()  # interpolate (works best at 1ms) # TODO
+            .drop("watchId", axis=1)
+            .reset_index(level=0)  # 'ungroup'
+            .groupby("watchId")
+            .resample(f"{resample_rate}ms")
+            .first()  # resample at desired rate
+            .drop("watchId", axis=1)
+            .reset_index(level=0)  # ungroup again
+        )
+        if save_data:
+            print(f"Saving resampled PPG data to {saved_data}")
+            df_out.to_parquet(saved_data)
     return df_out
 
 
-def PPG_find_peaks(df, config_dat):
+def PPG_find_peaks(df, config_dat, save_data=False):
     sample_rate = config_dat["pipelines"]["PPG_resample_rate"]
-    frequency = 1000 / sample_rate  # in Hz (samples/second)
-    df_out = pd.DataFrame()
-    for i, g in df.groupby("watchId"):
-        sig, info = nk.ppg_process(g["ppgRaw"], sampling_rate=frequency)
-        g = g.join(sig.set_index(g.index))
-        df_out = pd.concat([df_out, g])
+    saved_data = (
+        config_dat["directories"]["data"]["processed"]
+        + f"peaks_{sample_rate}ms.parquet"
+    )
+    if os.path.isfile(saved_data):
+        print(f"Using previously computed peaks data from {saved_data}")
+        df_out = pd.read_parquet(saved_data)
+    else:
+        print("Calculating peaks")
+        frequency = 1000 / sample_rate  # in Hz (samples/second)
+        df_out = pd.DataFrame()
+        for i, g in df.groupby("watchId"):
+            sig, info = nk.ppg_process(g["ppgRaw"], sampling_rate=frequency)
+            g = g.join(sig.set_index(g.index))
+            df_out = pd.concat([df_out, g])
+        if save_data:
+            print(f"Saving peaks data to {saved_data}")
+            df_out.to_parquet(saved_data)
     return df_out
