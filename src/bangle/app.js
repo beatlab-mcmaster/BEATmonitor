bfd7eb35490ca4e130b03c82daf622dc55fab9a3
Maya <38996333+mayabflannery@users.noreply.github.com>
2025-10-29 08:58:41 -0400

    update: Breaking changes! watches now have settings file; better watch feedback/logging

--- src/bangle/app.js
+++ src/bangle/app.js
@@ -1,60 +1,79 @@
-// BEATmonitor -- v0.051
+// BEATmonitor -- v0.100
 // Load storage module
 const storage = require("Storage");
 
 // Get device information
-const infoProgram = "BEATmonitor";
-const infoVersion = "v0.051";
-const infoSerial = process.env.SERIAL;
-const infoMAC = NRF.getAddress();
-const shortMAC = infoMAC.slice(-5).replace(":", "");
+const device = {
+  program: "BEATwatch",
+  version: "v0.100",
+  firmware: process.env.VERSION,
+  serial: process.env.SERIAL,
+  idMAC: NRF.getAddress(),
+  idShortMAC: NRF.getAddress().slice(-5).replace(":", ""),
+};
 
 // Change default BT advertisement
-NRF.setAdvertising({}, { name: `BEATLab ${shortMAC}` });
+NRF.setAdvertising({}, { name: `BEATwatch ${device.idShortMAC}` });
 
-let infoPhysicalID = (function () {
-  try {
-    return storage.readJSON("physicalID.json").PhysicalID;
-  } catch (e) {
-    return "ERR!"; // Error if cannot read ID
+// Default settings
+var settings = {
+  physicalId: "NA",
+  recordHR: true,
+  recordAccel: false,
+  allowManual: true,
+};
+
+// Store status
+var status = {
+  state: "WAIT",
+  hrmCollected: 0,
+  accelCollected: 0,
+  //Timestamp is set on record
+  startTimestamp: undefined,
+  batteryLife: E.getBattery(),
+  freeStorage: storage.getFree(),
+};
+
+function updateStatus() {
+  status.batteryLife = E.getBattery();
+  status.freeStorage = storage.getFree();
+}
+
+function updateSettings() {
+  if (storage.readJSON("beatSettings.json")) {
+    let fileSettings = storage.readJSON("beatSettings.json");
+    for (let setting in fileSettings) {
+      if (settings[setting]) {
+        settings[setting] = fileSettings[setting];
+      }
+    }
   }
-})();
+}
 
-var state = "WAIT";
-var samplesCollected = 0;
-var metaData;
-var data;
-// Timestamp is set on record
-var startTimestamp;
-
-let getFileData = function () {
+function getFileData() {
   let dt = new Date(Date.now());
   let shortDate = `${dt.toISOString().slice(5, 19)}`;
   let file = {
     File: {
-      Name: shortDate + "_" + shortMAC + "_" + infoPhysicalID,
-      Serial: infoSerial,
-      MAC: infoMAC,
-      PhysicalID: infoPhysicalID,
+      Name: shortDate + "_" + device.idShortMAC + "_" + settings.physicalId,
     },
+    DeviceInfo: device,
+    Settings: settings,
   };
   return file;
-};
+}
 
-let getMetaData = function (state) {
+function getMetaData(state) {
   let dt = new Date(Date.now());
   let data = {
+    Status: status,
     Record: {
-      State: state,
       DateTime: dt.toString(),
       UNIXTimeStamp: dt,
-      BatteryLife: E.getBattery(),
-      FreeStorage: storage.getFree(),
-      SamplesWritten: samplesCollected,
     },
   };
   return data;
-};
+}
 
 // ---------------------------- User Interface --------------------------------
 let drawTimeout;
@@ -66,7 +85,7 @@ const drawTouch = {
 };
 
 // Draw the watch face
-let draw = function () {
+function draw() {
   g.clear();
   g.setColor(0, 0, 0);
   // Write time -- only update every minute
@@ -80,31 +99,35 @@ let draw = function () {
   g.setFont("12x20");
   // Draw the program version
   g.drawString(
-    `${infoProgram}-${infoVersion}`,
+    `${device.program}-${device.version}`,
     drawTouch.x1 - 3,
     drawTouch.y1 - 45,
   );
 
   g.setFont("12x20");
   // Draw the physical device ID
-  g.drawString(`ID:\n${infoPhysicalID}`, drawTouch.x1 + 110, drawTouch.y1 + 25);
+  g.drawString(
+    `ID:\n${settings.physicalId}`,
+    drawTouch.x1 + 110,
+    drawTouch.y1 + 25,
+  );
 
   // This is the start button area
   g.drawRect(drawTouch.x1, drawTouch.y1, drawTouch.x2, drawTouch.y2);
 
   // Draw the device 'state'
-  g.drawString("> " + state, drawTouch.x1 + 5, drawTouch.y1 + 5);
+  g.drawString("> " + status.state, drawTouch.x1 + 5, drawTouch.y1 + 5);
 
   // Draw the number of samples collected in record
   g.drawString(
-    `Samples:\n${samplesCollected}`,
+    `Samples:\n${status.hrmCollected + status.accelCollected}`,
     drawTouch.x1 + 5,
     drawTouch.y1 + 25,
   );
 
   // Draw serial number and MAC address
   g.drawString(
-    `${infoSerial}\n  ${infoMAC}`,
+    `${device.serial}\n  ${device.idMAC}`,
     drawTouch.x1 - 3,
     drawTouch.y2 + 2,
   );
@@ -115,21 +138,24 @@ let draw = function () {
   drawTimeout = setTimeout(
     function () {
       drawTimeout = undefined;
+      updateStatus();
       draw();
     },
     60000 - (Date.now() % 60000),
   );
-};
+}
 
 // Respond to touch events...
 Bangle.on("touch", (button, xy) => {
-  if (
-    xy.x > drawTouch.x1 &&
-    xy.x < drawTouch.x2 &&
-    xy.y > drawTouch.y1 &&
-    xy.y < drawTouch.y2
-  ) {
-    startRecord();
+  if (settings.allowManual) {
+    if (
+      xy.x > drawTouch.x1 &&
+      xy.x < drawTouch.x2 &&
+      xy.y > drawTouch.y1 &&
+      xy.y < drawTouch.y2
+    ) {
+      startRecord();
+    }
   }
 });
 
@@ -154,15 +180,15 @@ setWatch(
 
 // ---------------------------- Record to watch -------------------------------
 // Button controls
-let startRecord = function () {
-  if (state == "WAIT") {
-    state = "START_RECORD";
-    console.log("Starting record");
+function startRecord() {
+  if (status.state == "WAIT") {
+    status.state = "START_RECORD";
+    Bluetooth.println("Starting record");
 
     // Get current watch info
     fileData = getFileData();
-    metaData = getMetaData(state);
-    startTimestamp = metaData.Record.UNIXTimeStamp;
+    metaData = getMetaData(status.state);
+    status.startTimestamp = metaData.Record.UNIXTimeStamp;
     // Create a file to store heart rate data
     data = storage.open(fileData.File.Name, "a");
     // Start by writing watch info
@@ -171,96 +197,92 @@ let startRecord = function () {
 
     // Turn on the heart rate sensor
     Bangle.setHRMPower(1);
-    state = "RECORDING";
+    status.state = "RECORDING";
     setNRF(1);
     draw();
   } else {
-    console.log("Not ready to start record");
+    Bluetooth.println("Not ready to start record");
   }
-};
+  Bluetooth.println(sendStatus());
+}
 
-let stopRecord = function () {
-  if (state == "RECORDING") {
-    state = "STOP_RECORD";
-    console.log("Stopping record");
+function stopRecord() {
+  if (status.state == "RECORDING") {
+    status.state = "STOP_RECORD";
+    Bluetooth.println("Stopping record");
 
     // Write end data
-    metaData = getMetaData(state);
+    metaData = getMetaData(status.state);
     data.write(JSON.stringify(metaData));
 
     // Reset record
     // Turn off the heart rate sensor
     Bangle.setHRMPower(0);
-    samplesCollected = 0;
-    state = "WAIT";
+    status.hrmCollected = 0;
+    status.accelCollected = 0;
+    status.state = "WAIT";
     setNRF(0);
     draw();
   } else {
-    console.log("No record to stop");
+    Bluetooth.println("No record to stop");
   }
-};
+  Bluetooth.println(sendStatus());
+}
 
 // ---------------------------- Send data stream ------------------------------
-let startStreaming = function () {
-  if (state == "WAIT") {
-    state = "START_STREAM";
-    console.log("Starting stream");
+function startStreaming() {
+  if (status.state == "WAIT") {
+    status.state = "START_STREAM";
+    Bluetooth.println("Starting stream");
 
     // Turn on the heart rate sensor
     Bangle.setHRMPower(1);
-    state = "STREAMING";
+    status.state = "STREAMING";
     setNRF(3);
     draw();
   } else {
-    console.log("Not ready to stream");
+    Bluetooth.println("Not ready to stream");
   }
-};
+}
 
-let stopStreaming = function () {
-  if ((state = "STREAMING")) {
-    state = "STOP_STREAM";
-    console.log("Stopping stream");
+function stopStreaming() {
+  if ((status.state = "STREAMING")) {
+    status.state = "STOP_STREAM";
+    Bluetooth.println("Stopping stream");
 
     // Turn off the heart rate sensor
     Bangle.setHRMPower(0);
-    samplesCollected = 0;
-    state = "WAIT";
+    status.hrmCollected = 0;
+    status.accelCollected = 0;
+    status.state = "WAIT";
     setNRF(0);
     draw();
   } else {
-    console.log("No stream to stop");
+    Bluetooth.println("No stream to stop");
   }
-};
+}
 
-// ---------------------------- Configure watch id ----------------------------
-let sendWatchId = function () {
-  if (state == "WAIT") {
-    print(`${infoPhysicalID}`);
-  } else {
-    print("[INFO] Watch is busy, cannot send ID");
-  }
-};
+// ---------------------------- Configure settings ----------------------------
+function sendSettings() {
+  Bluetooth.println(JSON.stringify(settings));
+}
 
-let setWatchId = function (watchID) {
-  if (state == "WAIT" && watchID != undefined) {
-    let info = {
-      PhysicalID: watchID,
-    };
-    storage.writeJSON("physicalID.json", info);
-    print(`SERIAL: ${process.env.SERIAL}`);
-    print(`MAC id: ${NRF.getAddress()}`);
-    print(`Watch id set to: ${watchID}`);
-    infoPhysicalID = watchID;
+function setSettings(newSettings) {
+  if (status.state == "WAIT" && newSettings != undefined) {
+    storage.writeJSON("beatSettings.json", newSettings);
+    updateSettings();
+    Bluetooth.println(JSON.stringify(device));
+    Bluetooth.println(sendSettings());
   } else {
-    print("[INFO] Watch is busy, cannot set ID!");
+    Bluetooth.println("[INFO] Watch is busy, cannot set ID!");
   }
-};
+}
 
 // ----------------------- Storage management / transfer ----------------------
-let sendStorage = function () {
-  if (state == "WAIT") {
+function sendStorage() {
+  if (status.state == "WAIT") {
     let files = { files: [] };
-    let storageFiles = storage.list(/(_W...)|(\.csv)/); // TODO: Allow for any watchname
+    let storageFiles = storage.list(/(\d.-\d.T\d.:\d.:\d._...._)/); // TODO: Allow for any watchname
     storageFiles.forEach((e) => {
       let f = storage.open(e.replace("\u0001", ""), "r");
       let l = f.getLength();
@@ -269,39 +291,39 @@ let sendStorage = function () {
         size: l,
       });
     });
-    print(JSON.stringify(files) + "[EOF]");
+    Bluetooth.println(JSON.stringify(files) + "[EOF]");
   } else {
-    print("[INFO] Watch is busy, cannot send storage!");
+    Bluetooth.println("[INFO] Watch is busy, cannot send storage!");
   }
-};
+}
 
-let deleteStorage = function (files) {
-  if (state == "WAIT") {
+function deleteStorage(files) {
+  if (status.state == "WAIT") {
     if (files === undefined) {
-      print("[INFO] No files to delete are specified!");
+      Bluetooth.println("[INFO] No files to delete are specified!");
     } else if (files == "all") {
-      let storageFiles = storage.list(/(_W...)|(\.csv)/); // TODO: Allow for any watchname
+      let storageFiles = storage.list(/(\d.-\d.T\d.:\d.:\d._...._)/);
       storageFiles.forEach((e) => {
-        print(`Deleting: ${e}`);
+        Bluetooth.println(`Deleting: ${e}`);
         storage.open(e.replace("\u0001", ""), "r").erase();
       });
     } else {
       storage.open(files, "r").erase();
-      print(`Deleting ${files}`);
+      Bluetooth.println(`Deleting ${files}`);
     }
   } else {
-    print("[INFO] Watch is busy, cannot delete file(s)!");
+    Bluetooth.println("[INFO] Watch is busy, cannot delete file(s)!");
   }
-};
+}
 
-let sendData = function (fileName) {
-  if (state == "WAIT") {
-    state = "SENDING";
+function sendData(fileName) {
+  if (status.state == "WAIT") {
+    status.state = "SENDING";
     setNRF(2);
     ts = Date.now();
     f = require("Storage").open(fileName, "r");
     var len = f.getLength(); // File length (size) in bytes
-    print(`[INFO] Sending file... ${fileName}`);
+    Bluetooth.println(`[INFO] Sending file... ${fileName}`);
     var prog = 0; // Keep track of the read/sent bytes
 
     var sendData = setInterval(() => {
@@ -315,25 +337,27 @@ let sendData = function (fileName) {
           " s]";
         clearInterval(progress); // stop sending progress
         clearInterval(sendData); // stop sending data
-        state = "WAIT";
+        status.state = "WAIT";
         setNRF(0);
       } else {
         prog += d.length; // update progress
       }
-      print(d); // Send data/message over bluetooth
+      Bluetooth.println(d); // Send data/message over bluetooth
     }, 1);
     // send 1 line/ms
     var progress = setInterval(() => {
       p = Math.round((prog / len) * 100); // Calculate progress
-      print("[Progress] " + p + "%  [" + prog + " of " + len + "] bytes");
+      Bluetooth.println(
+        "[Progress] " + p + "%  [" + prog + " of " + len + "] bytes",
+      );
     }, 1000); // Update once per second
   } else {
-    print("[INFO] Watch is busy, cannot send data!");
+    Bluetooth.println("[INFO] Watch is busy, cannot send data!");
   }
-};
+}
 
 // ---------------------------- Broadcast watch state -------------------------
-let setNRF = function (val) {
+function setNRF(val) {
   NRF.setAdvertising(
     {},
     {
@@ -341,25 +365,23 @@ let setNRF = function (val) {
       manufacturerData: JSON.stringify({ s: val }),
     },
   );
-};
+}
+
+// ---------------------------- Send status  ----------------------------------
+function sendStatus() {
+  Bluetooth.println(JSON.stringify(status));
+}
 
 // ---------------------------- Time sync / drift -----------------------------
-let syncTime = function (time) {
+function syncTime(time) {
   setTime(time);
   Bluetooth.println(getTime());
-};
+}
 
-let getDrift = function (serverTime) {
+function getDrift(serverTime) {
   let watchTime = getTime();
   Bluetooth.println(watchTime);
-};
-
-// ---------------------------- Vibration -------------------------------------
-let setVibrate = function (time, strength) {
-  Bangle.buzz(time, strength).then(() => {
-    print("[INFO] Vibration done");
-  });
-};
+}
 
 // ---------------------------- Record HR / PPG data --------------------------
 // Default interval is 80ms; this replaces the setInterval + period workaround
@@ -368,15 +390,15 @@ let setVibrate = function (time, strength) {
 
 var prevWriteTimestamp = 0;
 // This function will be called continuously while setHRMpower is on
-let getHR = function (hrm) {
+function getHR(hrm) {
   let now = Date.now();
   let diff = Math.round(now - prevWriteTimestamp);
 
   // We want a minimum of 35ms between samples; and filter unlikely heart rates
   if (diff > 35 && hrm.bpm > 30 && hrm.bpm < 240) {
-    if (state == "RECORDING") {
+    if (status.state == "RECORDING") {
       // Write diff from start of record to save space
-      let ts = Math.round(Date.now() - startTimestamp);
+      let ts = Math.round(Date.now() - status.startTimestamp);
       // Create row with unix time and hr data
       let obs = [
         ts,
@@ -387,7 +409,7 @@ let getHR = function (hrm) {
       ].join(",");
       // Write to file
       data.write(obs + "\n");
-    } else if (state == "STREAMING") {
+    } else if (status.state == "STREAMING") {
       let dbuf = new ArrayBuffer(19); // n = record size
       let d = new DataView(dbuf);
 
@@ -407,14 +429,38 @@ let getHR = function (hrm) {
 
       Bluetooth.println(d.buffer);
     }
-    samplesCollected++;
+    status.hrmCollected++;
     prevWriteTimestamp = now;
   }
-};
+}
+
+// ---------------------------- Record Acceleration ---------------------------
+
+function procAccel(xyz) {
+  if (settings.recordAccel & (status.state == "RECORDING")) {
+    let ts = Math.round(Date.now() - status.startTimestamp);
+    let obs = [
+      ts,
+      Math.round(xyz.x * 1000),
+      Math.round(xyz.y * 1000),
+      Math.round(xyz.z * 1000),
+      Math.round(xyz.mag * 1000),
+      Math.round(xyz.diff * 1000),
+    ].join(",");
+    data.write("A" + obs + "\n");
+    status.accelCollected++;
+  }
+}
 
 // ---------------------------- Initial function calls ------------------------
+// Get settings from file
+updateSettings();
+
 // Listen for HRM values
 Bangle.on("HRM-raw", getHR);
+
+// Listen for accelerometer values
+Bangle.on("accel", procAccel);
 
 // Call first draw to screen
 g.reset();
